7fe4e21e1bb31e9458a9816b8490185ac3fd9460
David Lorenz <info@activenode.de>
2018-08-26 13:22:19 +0200

    Restructured files

---
+++ sample/MyComponent.js
@@ -0,0 +1,53 @@
+Ocomponent.defineElement('x-component', (function(){
+    // to support old OWF code we could have something like:
+    // Util.requireDependencies()
+
+    return class extends Ocomponent {
+        onLoad() {
+            console.log('onLoad2');
+            // you could start fetching data in here
+        }
+
+        domReady() {
+            // well now you can do dom manipulation
+            // you could also do stuff like React.render in here. why not?
+            console.log('this is ready>>>', this.innerHTML);
+
+            this.require([
+                'x-other'
+            ]).then(() => this.start());
+        }
+
+        start() {
+            var other = document.createElement('x-other');
+            this.appendChild(other);
+        }
+    };
+}()));
+
+
+Ocomponent.defineElement('x-other', (function(){
+    // to support old code we could have something like:
+    // OldUtil.requireDependencies()
+
+    return class extends Ocomponent {
+        onLoad() {
+            console.log('onLoad2');
+            // you could start fetching data in here
+        }
+
+        domReady() {
+            // well now you can do dom manipulation
+            // you could also do stuff like React.render in here. why not?
+            console.log('this2 is ready>>>', this.innerHTML);
+
+            this.require(['x-component']).then(() => this.start());
+        }
+
+        start() {
+            this.innerHTML = 'Hello I am an automatically mounted x-other component';
+        }
+    };
+}()));
+
+// customElements.define('x-component', MyTest);
