4554868dbc4e66f4e91d871fe5eceb02830cf22f
David Lorenz <info@activenode.de>
2018-10-08 00:10:34 +0200

    Added event binding with on/off

--- src/polyfill.js
+++ src/polyfill.js
@@ -20,10 +20,45 @@ window.CustomEvent = CustomEvent;
  */
 
 var _OCO_INSTALL = (function(){
+  var _forEach = function(list, loopFunc) {
+    return [].forEach.call(list, loopFunc);
+  };
+
+  /**
+   * Iterates an object and provides key,value to the looping function
+   * @param {Object} obj - the object to iterate
+   * @param {Function<Key, Value>} loopFunc
+   */
+  var _loopObject = function(obj, loopFunc) {
+    return Object.keys(obj).forEach(function(key) {
+      loopFunc(key, obj[key]);
+    });
+  };
+
+  var _map = function(list, loopFunc) {
+    return [].map.call(list, loopFunc);
+  };
+
+  var _extend = function(/** srcObj, extendObj1, extendObj2, ... */) {
+    var srcObj = arguments[0];
+    var excludeKeys = this.excludeKeys ? this.excludeKeys : false;
+
+    for (var i=1; i < arguments.length; i++) {
+      _loopObject(arguments[i], function(key, value) {
+        if (!excludeKeys || excludeKeys.indexOf(key) === -1) {
+          srcObj[key] = value;
+        }
+      });
+    }
+
+    return srcObj;
+  };
+
   var NODE_TYPE_TEXT = 3;
   var OCO_ATTR = 'oco';
   var OCO_ATTR_NOT_SELECTOR = ':not(['+OCO_ATTR+'])';
   var NODE_UPGRADED_PROTO_KEY = '__oco_upgraded_node__';
+  var DEFAULT_EVENT_NAMESPACE = '!!';
 
   var _installer = function(_document){
     var _componentsDefined = [];
@@ -56,7 +91,7 @@ var _OCO_INSTALL = (function(){
 
           //TODO: process removedNodes first??
 
-          [].forEach.call(addedNodes, function(domElement) {
+          _forEach(addedNodes, function(domElement) {
             _PF_recursiveTreeUpgrade(domElement);
           });
         });
@@ -76,6 +111,7 @@ var _OCO_INSTALL = (function(){
       if (!Array.isArray(elementName)) {
         elementName = [elementName];
       }
+
       var componentsSelect = _document.querySelectorAll(
         elementName.map(function(n) {
           return n + OCO_ATTR_NOT_SELECTOR;
@@ -85,7 +121,7 @@ var _OCO_INSTALL = (function(){
         return [];
       }
 
-      return [].map.call(componentsSelect, function(_) { return _; });
+      return _map(componentsSelect, function(_) { return _; });
     }
 
     function _PF_upgradeNode(domElement) {
@@ -117,7 +153,7 @@ var _OCO_INSTALL = (function(){
 
       if (_containsUnitializedCustomElements(domElement)) {
         // dig deeper!
-        [].forEach.call(domElement.childNodes, function(childDomElement) {
+        _forEach(domElement.childNodes, function(childDomElement) {
           _PF_recursiveTreeUpgrade(childDomElement);
         });
       } // else: done
@@ -137,65 +173,176 @@ var _OCO_INSTALL = (function(){
       }
     }
 
-    _document.defineElement = function(elementName, methodsObj) {
-      var attachedCallbackOrig = methodsObj.attachedCallback;
-      var attachedCallback = function() {
-        this.setAttribute(OCO_ATTR, '');
+    _document.defineElement = (function(){
+      var getEventAndNamespace = function(eventName) {
+        var splittedEventString = eventName.split('.');
+        var namespaceString = splittedEventString.slice(1).join('.').trim();
+        return [splittedEventString[0], namespaceString === '' ? DEFAULT_EVENT_NAMESPACE : namespaceString];
+      };
 
-        if (attachedCallbackOrig) {
-          attachedCallbackOrig.call(this);
-        }
+      var getElementPrototypeExtensions = function() {
+        return {
+          __eventListeners: {},
+
+          on: function(eventName, listener) {
+            var eventAndNs = getEventAndNamespace(eventName);
+            var eventKey = eventAndNs[0];
+            var eventNs = eventAndNs[1];
+            var eventListenersRef = this.__eventListeners;
+
+            if (!eventListenersRef[eventKey]) {
+              this.__eventListeners[eventKey] = new Map;
+            }
+
+            if (!eventListenersRef[eventKey].has(eventNs)) {
+              eventListenersRef[eventKey].set(eventNs, []);
+            }
+
+            eventListenersRef[eventKey].get(eventNs).push(listener);
+            this.addEventListener(eventKey, listener);
+          },
+
+          off: function(eventName, listenerToRemove) {
+            var eventAndNs = getEventAndNamespace(eventName);
+            var eventKey = eventAndNs[0];
+            var eventNs = eventAndNs[1];
+            var eventListenersRef = this.__eventListeners;
+            var _this = this;
+
+            if (!eventListenersRef[eventKey]) {
+              return;
+            }
+
+            var deleteListeners = function(evtNamespace, listenersArray) {
+              if (Array.isArray(listenersArray)) {
+                var newListenersArray = [];
+                listenersArray.forEach(function(listenerFromArray) {
+                  if (!listenerToRemove || listenerToRemove === listenerFromArray) {
+                    // remove any listener
+                    _this.removeEventListener(eventKey, listenerFromArray);
+                  } else {
+                    // add back
+                    newListenersArray.push(listenerFromArray);
+                  }
+                });
+
+                if (newListenersArray.length > 0) {
+                  // return so that its not deleted!
+                  eventListenersRef[eventKey].set(eventNs, newListenersArray);
+                  return;
+                }
+              }
+
+              eventListenersRef[eventKey].delete(evtNamespace);
+            };
+
+            if (eventNs === DEFAULT_EVENT_NAMESPACE) {
+              // delete all!
+              eventListenersRef[eventKey].forEach(deleteListeners);
+              return;
+            }
+
+            if (!eventListenersRef[eventKey].has(eventNs)) {
+              return;
+            } // else: there is a value for this namespace!
+
+            console.log('wat this', eventKey, eventListenersRef[eventKey].get(eventNs));
+            deleteListeners(eventNs, eventListenersRef[eventKey].get(eventNs));
+          }
+        };
       };
 
       var getHtmlElementProto = function() {
         var prototype = Object.create(HTMLElement.prototype);
         prototype[NODE_UPGRADED_PROTO_KEY] = true;
-        Object.keys(methodsObj).forEach(function(key) {
-          prototype[key] = methodsObj[key]
-        });
 
-        prototype.attachedCallback = attachedCallback;
+        _forEach(arguments, function(obj) {
+          Object.keys(obj).forEach(function(key) {
+            prototype[key] = obj[key]
+          });
+        });
 
         return prototype;
-      }
+      };
 
-      if ('customElements' in window) {
-        var evalClass = eval('(function(){'
-        + 'return class extends HTMLElement {'
-        + 'constructor() {'
-        + ' super();'
-        + ' this[NODE_UPGRADED_PROTO_KEY] = true;'
-        + ' methodsObj.createdCallback.call(this);'
-        + '}'
+      return function(elementName, methodsObj) {
+        var attachedCallback = (function(_attachedCallback) {
+          return function() {
+            this.setAttribute(OCO_ATTR, '');
 
-        + 'connectedCallback() {'
-        + 'attachedCallback.call(this);'
-        + '}'
+            if (_attachedCallback) {
+              _attachedCallback.call(this);
+            }
+          };
+        }(methodsObj.attachedCallback));
 
-        + 'disconnectedCallback() {'
-        + 'if (\'detachedCallback\' in methodsObj) {'
-        + 'methodsObj.detachedCallback.call(this);'
-        + '}'
-        + '}'
-        + '}'
-        + '}());');
+        var detachedCallback = (function(_detachedCallback) {
+          return function() {
+            console.log('removing from DOM', this);
 
-        customElements.define(elementName, evalClass);
-      } else if ('registerElement' in _document) {
-        _document.registerElement(elementName, {
-          prototype: getHtmlElementProto()
-        });
-      } else {
-        _PF_componentsLibrary[elementName] = getHtmlElementProto();
+            if (_detachedCallback) {
+              _detachedCallback.call(this);
+            }
+          };
+        }(methodsObj.detachedCallback));
 
-        if (_document.readyState !== 'loading') {
-          var upgradeableNodes = _PF_getUpgradeableNodes(elementName);
-          upgradeableNodes.forEach(_PF_upgradeNode);
-        } // else: not needed because there is a global event listening!
-      }
+        var attachedDetachedMethods = {
+          attachedCallback: attachedCallback,
+          detachedCallback: detachedCallback
+        };
 
-      _componentsDefined.push(elementName);
-    };
+        if ('customElements' in window) {
+          var classDefinition = eval('(function(){'
+          + 'return class extends HTMLElement {'
+            + 'constructor() {'
+              + 'super();'
+              + 'this[NODE_UPGRADED_PROTO_KEY] = true;'
+              + 'methodsObj.createdCallback.call(this);'
+            + '}'
+
+            + 'connectedCallback() {'
+              + 'attachedCallback.call(this);'
+            + '}'
+
+            + 'disconnectedCallback() {'
+              + 'detachedCallback.call(this);'
+            + '}'
+          + '}'
+          + '}());');
+
+          var skipMethods = Object.keys(attachedDetachedMethods);
+          _extend.call(
+            { excludeKeys: skipMethods },
+            classDefinition.prototype,
+            getElementPrototypeExtensions(),
+            methodsObj
+          );
+
+          customElements.define(elementName, classDefinition);
+        } else {
+          var elementPrototype = getHtmlElementProto(
+            getElementPrototypeExtensions(),
+            methodsObj,
+            attachedDetachedMethods
+          );
+
+          if ('registerElement' in _document) {
+            _document.registerElement(elementName, {
+              prototype: elementPrototype
+            });
+          } else {
+            _PF_componentsLibrary[elementName] = elementPrototype;
+
+            if (_document.readyState !== 'loading') {
+              var upgradeableNodes = _PF_getUpgradeableNodes(elementName);
+              upgradeableNodes.forEach(_PF_upgradeNode);
+            } // else: not needed because there is a global event listening!
+          }
+        }
+
+        _componentsDefined.push(elementName);
+      };
+    }());
   };
 
   return _installer;
